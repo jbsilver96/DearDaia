ebcb46b61720f5e70bb19f2c816086eeb6d07725
Johnny Silver <76892070+jbsilver96@users.noreply.github.com>
2023-03-02 17:49:27 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const express = require("express");
+// configure dotenv
+require("dotenv").config();
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// import modules from OpenAI library
+const { Configuration, OpenAIApi } = require("openai");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+const openai = new OpenAIApi(configuration);
+
+const app = express();
+app.use(express.json());
+
+const port = process.env.PORT || 5000;
+
+app.post("/ask", async (req, res) => {
+    // getting prompt question from request
+    const prompt = req.body.prompt;
+  
+    try {
+        if (prompt == null) {
+          throw new Error("Uh oh, no prompt was provided");
+        }
+        // trigger OpenAI completion
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt,
+            temperature: 0.85,
+            max_tokens: 256,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0,
+    });
+
+    
+    console.log('Generated questions:', completion);
+    const completion = response.data.choices[0].text;
+    return res.status(200).json({
+        success: true,
+        message: completion,
+      });
+      
+      } catch (error) {
+        console.log(error.message);
+      }
+  });
+
+  app.listen(port, () => console.log(`Server is running on port ${port}!!`));
